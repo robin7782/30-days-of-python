600bef1aed5a80e52c6cf5e907eaf5a360d29816
Md. Nur Hossain Robin <77511095+robin7782@users.noreply.github.com>
2021-03-18 13:37:25 +0600

    Delete ex_11.py

--- ex_11.py
+++
@@ -1,4 +0,0 @@
-student = {'first_name':'robin','last_name': 'islam', 'gender': 'male', 'age': 22, 'martal status': 'single', 'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'], 'country':'Bangladesh', 'city': 'dhaka', 'address': 'gulshan'}
-gff = {'first_name':'robin','last_name': 'islam', 'gender': 'male', 'age': 22, 'martal status': 'single', 'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'], 'country':'Bangladesh', 'city': 'dhaka', 'address': 'gulshan'}
-gff.clear()
-print(gff)
