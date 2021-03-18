b32f9bc86c684f172d46dcb13dc148255a2a48c2
Md. Nur Hossain Robin <77511095+robin7782@users.noreply.github.com>
2021-03-18 13:27:01 +0600

    Add files via upload

---
+++ ex_3.py
@@ -0,0 +1,1 @@
+student = {'first_name':'robin','last_name': 'islam', 'gender': 'male', 'age': 22, 'martal status': 'single', 'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'], 'country':'Bangladesh', 'city': 'dhaka', 'address': 'gulshan'}
---
+++ ex_5.py
@@ -0,0 +1,3 @@
+student = {'first_name':'robin','last_name': 'islam', 'gender': 'male', 'age': 22, 'martal status': 'single', 'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'], 'country':'Bangladesh', 'city': 'dhaka', 'address': 'gulshan'}
+print(type('skills'))
+print(student['skills'])
