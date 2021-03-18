dc949b4d14642266a4311a4f88b46fc717e06b5b
Md. Nur Hossain Robin <77511095+robin7782@users.noreply.github.com>
2021-03-18 13:38:10 +0600

    Delete ex_6.py

--- ex_6.py
+++
@@ -1,3 +0,0 @@
-student = {'first_name':'robin','last_name': 'islam', 'gender': 'male', 'age': 22, 'martal status': 'single', 'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'], 'country':'Bangladesh', 'city': 'dhaka', 'address': 'gulshan'}
-student['skills'] = ['JavaScript', 'React', 'Node', 'MongoDB', 'Python','php','javacore']
-print(student['skills'])
