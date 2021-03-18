00dc2891c6c0a7db993450924ff6b24cfeb2fdbf
Md. Nur Hossain Robin <77511095+robin7782@users.noreply.github.com>
2021-03-18 13:38:45 +0600

    Delete ex_9.py

--- ex_9.py
+++
@@ -1,3 +0,0 @@
-student = {'first_name':'robin','last_name': 'islam', 'gender': 'male', 'age': 22, 'martal status': 'single', 'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'], 'country':'Bangladesh', 'city': 'dhaka', 'address': 'gulshan'}
-l = list(student.items())
-print(l)
